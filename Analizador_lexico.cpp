965d01fa9e5a215af175318e3d261e5dd323e3b9
said199 <said15pnd@gmail.com>
2021-04-03 00:48:35 -0600

    Evitar que numero racional tenga mas de dos puntos

--- Analizador_lexico.cpp
+++ Analizador_lexico.cpp
@@ -14,25 +14,25 @@ int i=0;
 
 FILE *fichero;
    fichero= fopen("lenguaje.txt","rt");
-  
+
      if (fichero==NULL) {// si fichero no falla coge un letra dle fichero
         cout << "Error al abrir el archivo lenguaje.txt"  <<  endl;
 
     } else {
-    
-    	
+
+
     	 for ( i = 0; i <200; i++)
     {
 
-        	
-        	 
+
+
 fscanf(fichero, "%s" , &textoExtraido[i]);
- 
-       	 
+
+
 string Cadena = textoExtraido[i];
 
 
-   
+
 string TablaSimbolos[9]={"mientras", "si", "sino", "fin","inicio", "entero", "racional", "entonces",
                           "imprima"};
 
@@ -65,10 +65,10 @@ while (longitud > i and Estado !=qe){
 			if(Simbolo >= '0' && Simbolo <= '9'){
 				Estado = q3;
 			}
-			
-			
+
+
 			break;
-			
+
 			case q1:
 				if(Simbolo >= 'a' && Simbolo <='z'|| Simbolo >= 'A' && Simbolo <='Z'){
 					Estado = q1;
@@ -77,7 +77,7 @@ while (longitud > i and Estado !=qe){
 					Estado = qe;
 				}
 				break;
-				
+
 			  case q2:
             //si es un digito
             if(Simbolo >= '0' && Simbolo <= '9'){
@@ -92,68 +92,86 @@ while (longitud > i and Estado !=qe){
             else{
 			if(Simbolo == '.'){
                     Estado= q4;
-                   
-				
+
+
                 }
             }
             break;
         case q4:
             //si es un digito
             if(Simbolo >= '0' && Simbolo <= '9' ){
-            
-            	
+
+
                 Estado= q5;
 				}
- 
+
             break;
-            
+
 	}
 	i++;
-	
- 
-    
+
+
+
 }
 
-          	
+int conta=0;
+ for(i=0; i<longitud; i++){
+  for(int j=0; j<longitud; j++){
+ if (Cadena[i]==Cadena[j*1]){
+ if(Cadena[i]=='-'){
+
+      conta++;
+         if(conta>2){
+
+         Estado=qe;
+
+         break;
+           }
+         }
+       }
+    }
+ }
+
+
 
 
 if(Estado==q1){
 	cout <<"\n ["<<Cadena<< "] es: Cadena aceptada";
-	
+
 	int j;
 	int TamanioArreglo=sizeof TablaSimbolos/ sizeof TablaSimbolos[0];
 	for(j=0; j< TamanioArreglo; j++){
 		if(Cadena.compare(TablaSimbolos[j])==0){
 			cout<<" y Palabra reservada" ;
 			break;
-			
+
 		}
-		
+
 	}
 }
 
 else if(Estado== q5){
-	
-	
+
+
 	cout <<"\n ["<<Cadena<< "] es: Numero Racional";
-	
+
 }else{
 
 	cout << "\n Existe un error en: "<<Cadena;
 	cout << " Programa detenido ";
 	break;
-	
+
 }
 
 
- 
+
 if(Cadena=="fin"){
-	
+
 		cout << "\n\n Fin del programa";
 
     break;
-    
-    
+
+
      }
  }
 }
